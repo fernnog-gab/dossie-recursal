4c7ae211a08b4384fdcdf6dcf9acd88d7eb256c0
fernnog-gab <fernandocnogueira1@gmail.com>
2026-03-21 08:19:54 -0400

    Update versions.js

--- versions.js
+++ versions.js
@@ -1,5 +1,14 @@
 const appVersions = [
     {
+        version: "1.0.1",
+        date: "21/03/2026",
+        features: [
+            "Correções de Nomenclatura para Gerador de Dossiê Recursal.",
+            "Inclusão da nova seção de Informações sobre Sentenças.",
+            "Correção do comportamento do botão de histórico de versões para não aparecer no arquivo exportado."
+        ]
+    },
+    {
         version: "1.0.0",
         date: "21/03/2026",
         features: [
